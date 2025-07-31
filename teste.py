421aaacd26e800d78bfcc93dd33a4cb8d0283eed
João Pedro Sampaio de Almeida <jpsam2011@gmail.com>
2025-07-31 19:32:32 -0300

    Básico em Python

---
+++ teste.py
@@ -0,0 +1,7 @@
+nome = imput("Qual o seu nome?")
+print(f"Seja bem vindo {nome}")
+idade = int(imput("Qual a sua idade?"))
+if idade >18:
+    print("Você é maior de idade! Navegue livremente")
+else:
+    print("Você é menor de idade! Não pode acessar esse site.")
